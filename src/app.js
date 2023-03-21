cb4015eee32d1b7532248003524b3a8933b77905
ANiroodh <parasharaniroodh@gmail.com>
2023-03-21 13:08:08 +0530

    setup app for deployment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const express = require('express')
+const port = process.env.PORT || 3000
 
 
 // //console.log(__dirname)
@@ -144,6 +145,6 @@ app.get('*', (req, res) => {
 //app.com
 //app.com/about
 
-app.listen(3000, () =>{
-    console.log('Server is up on port 3000.')
+app.listen(port, () =>{
+    console.log('Server is up on port ' +port)
 })
